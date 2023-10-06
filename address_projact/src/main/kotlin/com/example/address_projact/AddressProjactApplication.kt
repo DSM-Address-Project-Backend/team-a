c68b1bd316252b30619461f359399d223205274e
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-06 17:07:52 +0900

    🎉 :: 프로젝트 시작

---
+++ address_projact/src/main/kotlin/com/example/address_projact/AddressProjactApplication.kt
@@ -0,0 +1,11 @@
+package com.example.address_projact
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AddressProjactApplication
+
+fun main(args: Array<String>) {
+    runApplication<AddressProjactApplication>(*args)
+}
