7bef276b01c22ea9ef8feeeefdda8940d8876e46
jyk1029 <jyk102999@gmail.com>
2023-10-06 18:44:24 +0900

    ⚡️ :: 도로명 주소 Entity 설계

---
+++ src/main/kotlin/com/example/address_project/domain/road_addrss/domain/RoadAddress.kt
@@ -0,0 +1,29 @@
+package com.example.address_project.domain.road_addrss.domain
+import org.hibernate.annotations.DynamicInsert
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@DynamicInsert
+@Entity(name = "tbl_road_address")
+class RoadAddress(
+    @Id
+    @Column(columnDefinition = "BINARY(16)")
+    val id: UUID,
+
+    @Column(columnDefinition = "CHAR(26)", nullable = false)
+    val managementNumber: String,
+
+    @Column(columnDefinition = "INT(5)", nullable = false)
+    val buildingNum: Int,
+
+    @Column(columnDefinition = "INT(5)", nullable = false)
+    val buildingSubNum: Int,
+
+    @Column(columnDefinition = "CHAR(5)", nullable = false)
+    val postNumber: String,
+
+    @Column(columnDefinition = "BIT(5)", nullable = false)
+    val isGiveDetailAddress: Boolean,
+)
