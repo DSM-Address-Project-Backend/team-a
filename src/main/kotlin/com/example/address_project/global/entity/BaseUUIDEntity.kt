bee8b2d67abfd99aa21c777d453c52c3b82f9e20
alsco39 <minchaeg85@gmail.com>
2023-10-06 18:50:07 +0900

    📑 :: BaseUUIDEntity

---
+++ src/main/kotlin/com/example/address_project/global/entity/BaseUUIDEntity.kt
@@ -0,0 +1,18 @@
+package com.example.address_project.global.entity
+
+import org.hibernate.annotations.GenericGenerator
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseUUIDEntity(
+
+    @Id
+    @GeneratedValue(generator = "uuid4")
+    @GenericGenerator(name = "uuid4", strategy = "uuid4")
+    @Column(columnDefinition = "BINARY(16)")
+    val id: UUID = UUID.randomUUID(),
+)
