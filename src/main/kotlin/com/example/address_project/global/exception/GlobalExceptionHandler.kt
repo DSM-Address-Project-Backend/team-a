1352f6574db96dd114ae3b62f73c0bd066a8e0c3
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-25 21:22:15 +0900

    exception fliter, handler 추가

---
+++ src/main/kotlin/com/example/address_project/global/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,20 @@
+package com.example.address_project.global.exception
+
+import com.example.address_project.global.error.BusinessException
+import com.example.address_project.global.error.ErrorResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.time.LocalDateTime
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException::class)
+    fun handlingBusinessException(e: BusinessException): ResponseEntity<ErrorResponse> {
+        val code = e.errorCode
+        val errorResponse = ErrorResponse(code.status, code.message, LocalDateTime.now()) // ErrorResponse 객체 생성
+        return ResponseEntity(errorResponse, HttpStatus.valueOf(code.status)) // 생성한 객체 반환
+    }
+}
