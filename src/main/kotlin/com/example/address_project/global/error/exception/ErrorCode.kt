5b50dc94276d99b7279e06619d515a9729d259c7
alsco39 <minchaeg85@gmail.com>
2023-10-29 15:39:23 +0900

    ♻️ :: Feign ErrorCode 추가

--- src/main/kotlin/com/example/address_project/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/address_project/global/error/exception/ErrorCode.kt
@@ -4,4 +4,8 @@ enum class ErrorCode(
     val status: Int,
     val message: String
 ) {
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+
+    FEIGN_BAD_REQUEST(400, "Feign Bad Request"),
+    FEIGN_INTERNAL_SERVER_ERROR(500, "Feign Internal Server Error")
 }
