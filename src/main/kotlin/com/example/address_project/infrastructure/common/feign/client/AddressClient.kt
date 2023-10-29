ab65683607110e66bb2fb8df458cc2c45670497a
alsco39 <minchaeg85@gmail.com>
2023-10-29 15:39:51 +0900

    📑 :: Feign AddressClient

---
+++ src/main/kotlin/com/example/address_project/infrastructure/common/feign/client/AddressClient.kt
@@ -0,0 +1,22 @@
+package com.example.address_project.infrastructure.common.feign.client
+
+import com.example.address_project.infrastructure.common.feign.client.dto.response.AddressZipFileResponse
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@FeignClient(name = "AddressClient", url = "https://business.juso.go.kr")
+interface AddressClient {
+
+    @GetMapping("/download.do")
+    fun getIsHomecomingDay(
+        @RequestParam reqType: String,
+        @RequestParam regYmd: Int,
+        @RequestParam ctprvnCd: Int,
+        @RequestParam stdde: Int,
+        @RequestParam fileName: String,
+        @RequestParam intNum: Int,
+        @RequestParam intFileNo: Int,
+        @RequestParam realFileName: String
+    ) : AddressZipFileResponse
+}
