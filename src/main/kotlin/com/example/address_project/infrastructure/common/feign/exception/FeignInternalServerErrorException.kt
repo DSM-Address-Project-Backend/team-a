2040b5b134a1ea0c1ad09aba57d2101a4ef837ee
rudeh2926 <dokyungkim2926@gmail.com>
2023-10-30 09:29:06 +0900

    object로 변경

--- src/main/kotlin/com/example/address_project/infrastructure/common/feign/exception/FeignInternalServerErrorException.kt
+++ src/main/kotlin/com/example/address_project/infrastructure/common/feign/exception/FeignInternalServerErrorException.kt
@@ -3,4 +3,4 @@ package com.example.address_project.infrastructure.common.feign.exception
 import com.example.address_project.global.error.exception.BusinessException
 import com.example.address_project.global.error.exception.ErrorCode
 
-class FeignInternalServerErrorException : BusinessException(ErrorCode.FEIGN_INTERNAL_SERVER_ERROR)
+object FeignInternalServerErrorException : BusinessException(ErrorCode.FEIGN_INTERNAL_SERVER_ERROR)
