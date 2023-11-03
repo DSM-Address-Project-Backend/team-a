90750cb36447adc217d064ba9bda4a52f8e07730
rudeh2926 <dokyungkim2926@gmail.com>
2023-11-03 23:20:20 +0900

    파일 작성

--- src/main/kotlin/com/example/address_project/domain/road_code/service/RoadCodeClientService.kt
+++ src/main/kotlin/com/example/address_project/domain/road_code/service/RoadCodeClientService.kt
@@ -1,5 +1,27 @@
 package com.example.address_project.domain.road_code.service
 
-class RoadCodeClientService {
-    //TODO 파싱 서비스
+import com.example.address_project.domain.road_code.domain.repository.RoadCodeRepository
+import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.File
+import java.io.Reader
+import javax.transaction.Transactional
+
+@Service
+class RoadCodeClientService(
+    private val roadCodeRepository: RoadCodeRepository
+) {
+    @Transactional
+    fun fileReaderRoadCode() {
+        val file = File("") // 파일명 추가하기
+        if (file.exists()) {
+            val reader = BufferedReader(file.reader())
+            try {
+                file.forEachLine { line ->
+                    val line = line.split("\t")
+                    
+                }
+            }
+        }
+    }
 }
