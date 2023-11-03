5f71664435e8ee1232ad28d162fca8c6d921ce8a
rudeh2926 <dokyungkim2926@gmail.com>
2023-11-03 23:33:15 +0900

    변수명 변경

--- src/main/kotlin/com/example/address_project/domain/road_code/service/RoadCodeClientService.kt
+++ src/main/kotlin/com/example/address_project/domain/road_code/service/RoadCodeClientService.kt
@@ -4,7 +4,7 @@ import com.example.address_project.domain.road_code.domain.repository.RoadCodeRe
 import org.springframework.stereotype.Service
 import java.io.BufferedReader
 import java.io.File
-import java.io.Reader
+import java.io.FileReader
 import javax.transaction.Transactional
 
 @Service
@@ -15,11 +15,14 @@ class RoadCodeClientService(
     fun fileReaderRoadCode() {
         val file = File("") // 파일명 추가하기
         if (file.exists()) {
-            val reader = BufferedReader(file.reader())
+            val reader = BufferedReader(FileReader(file, Charsets.UTF_8))
             try {
                 file.forEachLine { line ->
-                    val line = line.split("\t")
-                    
+                    val spacing = line.split("|")
+
+
+
+                    roadCodeRepository.save()
                 }
             }
         }
