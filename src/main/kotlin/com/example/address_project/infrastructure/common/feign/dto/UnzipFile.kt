73c74e139047f7a4c80485e3eee12d02006b7ba2
min56 <minchaeg85@gmail.com>
2023-11-13 23:14:31 +0900

    📑 :: UnzipFile

---
+++ src/main/kotlin/com/example/address_project/infrastructure/common/feign/dto/UnzipFile.kt
@@ -0,0 +1,9 @@
+package com.example.address_project.infrastructure.common.feign.dto
+
+data class UnzipFile (
+    val reqType: String,
+    val zipFilePath: String,
+    val unzipTargetDirectoryPath: String,
+    val year: Int,
+    val month: String,
+)
