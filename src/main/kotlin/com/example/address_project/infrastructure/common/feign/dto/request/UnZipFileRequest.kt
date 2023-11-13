0ff8b49e3cc048451904a30a4f4e25f420a63a51
min56 <minchaeg85@gmail.com>
2023-11-13 23:13:23 +0900

    📑 :: UnZipFileRequest

---
+++ src/main/kotlin/com/example/address_project/infrastructure/common/feign/dto/request/UnZipFileRequest.kt
@@ -0,0 +1,9 @@
+package com.example.address_project.infrastructure.common.feign.dto.request
+
+data class UnZipFileRequest(
+    val reqType: String,
+    val year: String,
+    val month: String,
+    val fileName: String,
+    val realFileName: String
+)
