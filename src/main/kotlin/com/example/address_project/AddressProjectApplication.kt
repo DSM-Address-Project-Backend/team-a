b94415f58f5c99a2f1ff6deca85adcf6346252f6
min56 <minchaeg85@gmail.com>
2023-11-13 23:10:41 +0900

    ♻️ :: 어노테이션 삭제

--- src/main/kotlin/com/example/address_project/AddressProjectApplication.kt
+++ src/main/kotlin/com/example/address_project/AddressProjectApplication.kt
@@ -1,12 +1,8 @@
 package com.example.address_project
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.openfeign.EnableFeignClients
 
-@EnableFeignClients
-@EnableBatchProcessing
 @SpringBootApplication
 class AddressProjectApplication
 
