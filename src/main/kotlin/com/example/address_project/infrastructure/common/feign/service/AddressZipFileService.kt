7346aad434861c006202b6df874d302daba0de64
alsco39 <minchaeg85@gmail.com>
2023-11-15 18:39:59 +0900

    ♻️ :: 메소드명 변경

--- src/main/kotlin/com/example/address_project/infrastructure/common/feign/service/AddressZipFileService.kt
+++ src/main/kotlin/com/example/address_project/infrastructure/common/feign/service/AddressZipFileService.kt
@@ -23,7 +23,7 @@ class AddressZipFileService(
     private val addressClient: AddressClient,
 ) {
 
-    fun unZipFile(unZipFile: UnzipFile) {
+    fun AddressfileWriter(unZipFile: UnzipFile) {
         Files.write(
             Paths.get(unZipFile.zipFilePath), getAddressInfo(unZipFile.reqType, unZipFile.year, unZipFile.month)
         )
