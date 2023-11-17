428575cdfbd0a110ec07ce512e6722e09ac047e2
도경 <dokyungkim2926@gmail.com>
2023-11-17 23:15:37 +0900

    로직 작성

--- src/main/kotlin/com/example/address_project/domain/road_code/service/FileReaderRoadCodeService.kt
+++ src/main/kotlin/com/example/address_project/domain/road_code/service/FileReaderRoadCodeService.kt
@@ -6,19 +6,62 @@ import com.example.address_project.domain.road_code.exception.FileInternalError
 import com.example.address_project.domain.road_code.exception.FileNotFoundException
 import com.example.address_project.infrastructure.common.feign.dto.UnzipFile
 import com.example.address_project.infrastructure.common.feign.service.AddressZipFileService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
 import javax.transaction.Transactional
 
 @Service
-class FileReaderRoadCodeService(
+class FileReaderRoadCodeService (
+    @Value("\${upload.dir}")
+    private  var uploadDir: String,
     private val roadCodeRepository: RoadCodeRepository,
     private val addressZipFileService: AddressZipFileService
 ) {
-    @Transactional
-    fun fileReaderRoadCode() {
 
+    @org.springframework.transaction.annotation.Transactional
+    fun readerKorea(reqType: String, year: String, month: String, fileName: String, realFileName: String, file: MultipartFile, unzipFile: UnzipFile) {
+
+        val filePath = saveFile(file)
+
+        addressZipFileService.addressFileWriter(unzipFile)
+
+        saveDatabase(filePath)
+    }
+
+    private fun saveFile(file: MultipartFile): String {
+        val uploadPath = File(uploadDir)
+
+        if (!uploadPath.exists()) {
+            uploadPath.mkdirs()
+        }
+
+        val targetLocation = File(uploadPath, file.originalFilename!!)
+
+        file.transferTo(targetLocation)
+
+        return targetLocation.absolutePath
+    }
+
+    private fun saveDatabase(filePath: String) {
+        val lines = File(filePath).readLines()
+        for (line in lines) {
+            val columns = line.split("|")
+            val roadCode = RoadCode(
+                roadName = columns[4],
+                cityName = columns[6],
+                sggName = columns[7],
+                emdName = columns[10],
+                emdSection = columns[8].toBoolean(),
+                emdCode = columns[1],
+                emdSerialNum = columns[2],
+                roadNameCode = columns[1] + columns[2]
+            )
+
+            roadCodeRepository.save(roadCode)
+        }
     }
 }
