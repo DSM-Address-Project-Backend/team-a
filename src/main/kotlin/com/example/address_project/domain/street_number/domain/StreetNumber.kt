e082a48c5c5b4a1eb02f926ba866d0a27ff98a4c
dkflfkd53 <b57062562@gmail.com>
2023-11-17 16:27:43 +0900

    지번 entity 수정

--- src/main/kotlin/com/example/address_project/domain/street_number/domain/StreetNumber.kt
+++ src/main/kotlin/com/example/address_project/domain/street_number/domain/StreetNumber.kt
@@ -13,28 +13,40 @@ import javax.persistence.Table
 @Table(name = "tbl_street_number")
 @Entity
 class StreetNumber (
-    @Column(columnDefinition = "CHAR(10)", nullable = false)
-    val legalDistrictCode: String,
+        @Column(columnDefinition = "CHAR(10)", nullable = false)
+        val legalDistrictCode: String? = null,
 
-    @Column(columnDefinition = "VARCHAR(40)", nullable = false)
-    val cityName: String,
+        @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+        val cityName: String? = null,
 
-    @Column(columnDefinition = "VARCHAR(40)", nullable = false)
-    val siGunGuName: String,
+        @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+        val siGunGuName: String? = null,
 
-    @Column(columnDefinition = "VARCHAR(40)", nullable = false)
-    val legalEmdName: String,
+        @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+        val legalEmdName: String? = null,
 
-    @Column(columnDefinition = "VARCHAR(40)", nullable = false)
-    val legalName: String,
+        @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+        val legalName: String? = null,
 
-    @Column(columnDefinition = "INT(4)", nullable = false)
-    val areaNum: Int,
+        @Column(columnDefinition = "INT(4)", nullable = false)
+        val areaNum: Int? = null,
 
-    @Column(columnDefinition = "INT(4)", nullable = false)
-    val areaSubNum: Int,
+        @Column(columnDefinition = "INT(4)", nullable = false)
+        val areaSubNum: Int? = null,
 
-    roadAddress: RoadAddress
+        @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+        val engCityName: String? = null,
+
+        @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+        val engSiGunGuName: String? = null,
+
+        @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+        val engLegalEmdName: String? = null,
+
+        @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+        val engLegalName: String? = null,
+
+    roadAddress: RoadAddress? = null
 ) : BaseUUIDEntity() {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "road_address_id", columnDefinition = "BINARY(16)", nullable = false)
