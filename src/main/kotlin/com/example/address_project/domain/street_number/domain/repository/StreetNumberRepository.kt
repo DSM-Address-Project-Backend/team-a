a4fc96346670d4a3ac18481ff29230e63eb168a7
dkflfkd53 <b57062562@gmail.com>
2023-11-29 20:20:30 +0900

    와일드 카드 삭제

--- src/main/kotlin/com/example/address_project/domain/street_number/domain/repository/StreetNumberRepository.kt
+++ src/main/kotlin/com/example/address_project/domain/street_number/domain/repository/StreetNumberRepository.kt
@@ -2,7 +2,7 @@ package com.example.address_project.domain.street_number.domain.repository
 
 import com.example.address_project.domain.street_number.domain.StreetNumber
 import org.springframework.data.repository.CrudRepository
-import java.util.*
+import java.util.UUID
 
 interface StreetNumberRepository : CrudRepository<StreetNumber, UUID> {
 }
