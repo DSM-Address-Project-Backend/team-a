ddc129a423e19ae9fc3f03f3f0c2b78126546f6b
도경 <dokyungkim2926@gmail.com>
2023-12-03 21:22:40 +0900

    value 지정

--- src/main/kotlin/com/example/address_project/domain/enums/Type.kt
+++ src/main/kotlin/com/example/address_project/domain/enums/Type.kt
@@ -1,7 +1,9 @@
 package com.example.address_project.domain.enums
 
-enum class Type {
-    roadName,
-    localNumber,
-    postOfficeBox
+enum class Type(
+    val number: Int
+) {
+    ROAD_NAME(0),
+    LOCAL_NUMBER(1),
+    POST_OFFICE_BOX(2)
 }
