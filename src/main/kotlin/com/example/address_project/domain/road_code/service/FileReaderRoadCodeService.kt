0cc3b5594aa570cdf702ff29f214f837125f57d9
min56 <minchaeg85@gmail.com>
2023-12-29 22:14:46 +0900

    ♻️ :: filepath 수정

--- src/main/kotlin/com/example/address_project/domain/road_code/service/FileReaderRoadCodeService.kt
+++ src/main/kotlin/com/example/address_project/domain/road_code/service/FileReaderRoadCodeService.kt
@@ -1,29 +1,21 @@
 package com.example.address_project.domain.road_code.service
 
 import com.example.address_project.domain.enums.Type
-import com.example.address_project.domain.facade.SaveFileFacade
 import com.example.address_project.domain.road_code.domain.RoadCode
 import com.example.address_project.domain.road_code.domain.repository.RoadCodeRepository
-import com.example.address_project.infrastructure.common.feign.dto.UnzipFile
 import com.example.address_project.infrastructure.common.feign.service.AddressZipFileService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.multipart.MultipartFile
 import java.io.File
 
 @Service
 class FileReaderRoadCodeService (
     private val roadCodeRepository: RoadCodeRepository,
-    private val addressZipFileService: AddressZipFileService,
-    private val saveFileFacade: SaveFileFacade
 ) {
 
     @Transactional
-    fun readerRoadCodeKorea(file: MultipartFile, unzipFile: UnzipFile) {
-
-        val filePath = saveFileFacade.saveFile(file)
-
-        addressZipFileService.addressFileWriter(unzipFile)
+    fun readerRoadCodeKorea(file: File) {
+        val filePath = file.path
 
         val lines = File(filePath).readLines()
         for (line in lines) {
--- src/main/kotlin/com/example/address_project/domain/street_number/service/FileReaderStreetNumberService.kt
+++ src/main/kotlin/com/example/address_project/domain/street_number/service/FileReaderStreetNumberService.kt
@@ -1,29 +1,21 @@
 package com.example.address_project.domain.street_number.service
 
 import com.example.address_project.domain.enums.Type
-import com.example.address_project.domain.facade.SaveFileFacade
 import com.example.address_project.domain.street_number.domain.StreetNumber
 import com.example.address_project.domain.street_number.domain.repository.StreetNumberRepository
-import com.example.address_project.infrastructure.common.feign.dto.UnzipFile
-import com.example.address_project.infrastructure.common.feign.service.AddressZipFileService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.multipart.MultipartFile
 import java.io.File
 
 @Service
 class FileReaderStreetNumberService (
-        private val roadCodeRepository: StreetNumberRepository,
-        private val saveFileFacade: SaveFileFacade,
-        private val addressZipFileService: AddressZipFileService
+        private val roadCodeRepository: StreetNumberRepository
 ) {
 
     @Transactional
-    fun readerStreetNumberKorea(file: MultipartFile, unzipFile: UnzipFile) {
+    fun readerStreetNumberKorea(file: File) {
 
-        val filePath = saveFileFacade.saveFile(file)
-
-        addressZipFileService.addressFileWriter(unzipFile)
+        val filePath = file.path
 
         val lines = File(filePath).readLines()
         for (line in lines) {
