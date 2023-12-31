b095b244ba3a672e5d6c79116158c07feb13dc0b
도경 <dokyungkim2926@gmail.com>
2023-12-31 19:42:41 +0900

    코드 리펙토링

--- src/main/kotlin/com/example/address_project/domain/post_office_box/service/FileReaderReaderPostOfficeBoxService.kt
+++ src/main/kotlin/com/example/address_project/domain/post_office_box/service/FileReaderReaderPostOfficeBoxService.kt
@@ -3,7 +3,6 @@ package com.example.address_project.domain.post_office_box.service
 import com.example.address_project.domain.enums.Type
 import com.example.address_project.domain.post_office_box.domain.repository.PostOfficeBoxRepository
 import com.example.address_project.domain.post_office_box.domain.PostOfficeBox
-import com.example.address_project.infrastructure.common.feign.service.AddressZipFileService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.io.File
@@ -11,7 +10,6 @@ import java.io.File
 @Service
 class FileReaderReaderPostOfficeBoxService(
     private val postOfficeBoxRepository: PostOfficeBoxRepository,
-    private val addressZipFileService: AddressZipFileService
 ) {
 
     @Transactional
